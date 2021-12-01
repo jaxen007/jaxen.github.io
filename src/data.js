ab629c3cc71b3f7fb0351644308c85ea4186fd34
jaxen007 <46900654+jaxen007@users.noreply.github.com>
2021-12-01 11:51:18 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const projects = [
+    {
+      title: "HYBRID APPROACH FOR TIME SERIES PREDICTION",
+      subtitle: "Machine Learning",
+      description:
+        "Implemented a hybrid approach for Time Series Prediction using AutoRegressive Integrated Moving Average and Clustering.",
+      image: "./project-1.gif",
+      link: "https://github.com/jaxen007",
+    },
+    {
+      title: "FAKE NEWS DETECTION",
+      subtitle: "Machine Learning",
+      description:
+        "TfidVectorizer was done on our dataset to convert data to matrix of Tf-IDF features.Using sklearn,PassiveAgressive Classifier was initialized to fit the"
+        +"model,model obtained was 92 Percent accurate .",
+      image: "./project-2.gif",
+      link: "https://github.com/jaxen007",
+    }
+  ];
+
+
+  export const skills=["React","Java","C++","C","DBMS","Core Java","Python","ElasticSearch","Redis","Mysql","Eclipse","Postman","Kibana","docker","Maven","Servlet"];
+
+
+
+
+  export const testimonials=[
+    {
+      name: "Jatin Sharma",
+      quote: "very good programming skills and logic Building",
+      image: "./Project-4.gif",
+      company: "flipkart",
+    },
+    {
+      name: "shashank",
+      quote: "very good at backend technologies ",
+      image: "./Project-3.gif",
+      company: "jio private limited",
+    }
+  ]
